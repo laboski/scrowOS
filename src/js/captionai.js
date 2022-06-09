34dea26b26b21e3c54c38e512ccb09e20d2fc06d
Precious Tom <tomprezine@gmail.com>
2022-06-09 14:03:43 +0100

    _init push_

---
+++ src/js/captionai.js
@@ -0,0 +1,14 @@
+// class CaptionAI {
+//     constructor(name, version) {
+//       this.name = name;
+//       this.version = version;
+//     }
+//     // Getter
+//     get toolname() {
+//       return this.name();
+//     }
+//     // Method
+//     sampleMethod() {
+//       return "This tool's name is " + this.name ;
+//     }
+// }
---
+++ src/js/equitycalc.js
@@ -0,0 +1,14 @@
+// class EquityCalculator {
+//     constructor(name, version) {
+//       this.name = name;
+//       this.version = version;
+//     }
+//     // Getter
+//     get toolname() {
+//       return this.name();
+//     }
+//     // Method
+//     sampleMethod() {
+//       return "This tool's name is " + this.name ;
+//     }
+// }
